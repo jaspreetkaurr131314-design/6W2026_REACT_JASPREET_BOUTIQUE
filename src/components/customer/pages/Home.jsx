789451a7aac7b7d81557f584b04678d48629b568
jaspreetkaurr131314-design <jaspreet.kaurr131314@gmail.com>
2026-08-01 13:47:28 +0530

    change

--- src/components/customer/pages/Home.jsx
+++ src/components/customer/pages/Home.jsx
@@ -69,7 +69,7 @@ export default function Home() {
         {/* .site-mobile-menu */}
         <div
           className="site-blocks-cover overlay"
-          style={{ backgroundImage: 'url("public/background.jpg")' }}
+          style={{ backgroundImage: 'url("/background.jpg")' }}
           data-aos="fade"
           data-stellar-background-ratio="0.5"
         >
