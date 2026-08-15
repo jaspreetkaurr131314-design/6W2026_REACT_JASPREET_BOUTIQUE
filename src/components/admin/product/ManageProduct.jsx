d6a5f623726755fa07c5e023950684208dd674db
jaspreetkaurr131314-design <jaspreet.kaurr131314@gmail.com>
2026-08-15 18:55:19 +0530

    update

--- src/components/admin/product/ManageProduct.jsx
+++ src/components/admin/product/ManageProduct.jsx
@@ -81,7 +81,7 @@ export default function ManageProduct() {
             {/* .site-mobile-menu */}
             <div
                 className="site-blocks-cover inner-page-cover overlay"
-                style={{ backgroundImage: 'url("images/hero_bg_1.jpg")' }}
+                style={{ backgroundImage: 'url("/background.jpg")' }}
                 data-aos="fade"
                 data-stellar-background-ratio="0.5"
             >
